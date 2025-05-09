1998120ff19b72bbf342ad1964380ece8e476582
contability <shouldbnewb@gmail.com>
2025-05-10 02:17:41 +0900

    (add) @react-native-cookies/cookies 패키지 추가 및 로그인 버튼 기능 개선

--- components/LoginButton.tsx
+++ components/LoginButton.tsx
@@ -1,9 +1,11 @@
-import {useNavigation} from '@react-navigation/native';
+import {useIsFocused, useNavigation} from '@react-navigation/native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
-import React, {useCallback} from 'react';
+import React, {useCallback, useContext, useEffect, useState} from 'react';
 import {TouchableOpacity} from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import {RootStackParamList, RouteNames} from '../routes';
+import CookieManager from '@react-native-cookies/cookies';
+import {WebViewContext} from './WebViewProvider';
 
 // const styles = StyleSheet.create({
 //   safearea: {flex: 1}
@@ -12,16 +14,42 @@ import {RootStackParamList, RouteNames} from '../routes';
 type Props = NativeStackNavigationProp<RootStackParamList>;
 
 const LoginButton = () => {
+  const context = useContext(WebViewContext);
   const navigation = useNavigation<Props>();
+  const isFocused = useIsFocused();
 
-  const isLoggedIn = false;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const iconName = isLoggedIn ? 'logout' : 'login';
+  console.log('🚀 ~ LoginButton ~ isLoggedIn:', isLoggedIn);
 
   const onPressLogin = useCallback(() => {
     navigation.navigate(RouteNames.LOGIN);
   }, [navigation]);
 
-  const onPressLogout = useCallback(() => {}, []);
+  const onPressLogout = useCallback(async () => {
+    await CookieManager.clearAll(true);
+    setIsLoggedIn(false);
+    if (context?.webViewRefs.current != null) {
+      context?.webViewRefs.current.forEach(webView => {
+        webView.reload();
+      });
+    }
+  }, [context?.webViewRefs]);
+
+  useEffect(() => {
+    if (isFocused) {
+      // CookieManager.get 메서드의 파라미터:
+      // param1. 쿠키를 가져올 URL 도메인 (문자열)
+      // param2. 모든 사용 가능한 쿠키 속성을 가져올지 여부 (boolean, true면 모든 속성 포함)
+      CookieManager.get('https://.naver.com/', true).then(cookie => {
+        if (cookie.NID_SES) {
+          setIsLoggedIn(true);
+        } else {
+          setIsLoggedIn(false);
+        }
+      });
+    }
+  }, [isFocused]);
   return (
     <TouchableOpacity onPress={isLoggedIn ? onPressLogout : onPressLogin}>
       <MaterialCommunityIcons name={iconName} color="white" size={24} />
--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -26,7 +26,7 @@ const LoginScreen = () => {
       <WebView
         source={{uri: LOGIN_URL}}
         onNavigationStateChange={event => {
-          if (event.url === 'https://m.naver.com') {
+          if (event.url === 'https://m.naver.com/') {
             // 로그인 후에 그냥 goBack을 해주게 되면 로그인은 되었지만 이전 화면을 그대로 가져오기 때문에 state 갱신이 안된 스크린을 보게 됨.
             // webView context에 등록된 스크린들 모두 reload 후에 goback 적용.
             context?.webViewRefs.current.forEach(webView => {
